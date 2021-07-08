41650d9cadd0c3741ff796cdb62f4cd5c514c670
IndranilC <chakraborty.indranil@gmail.com>
2021-07-08 12:22:14 +0530

    These are the initial skeleton files of the playground BDD project

---
+++ com.emids.cucumber.examples/src/test/java/com/ebay/scenario/based/steps/Ebay_Home.java
@@ -0,0 +1,24 @@
+package com.ebay.scenario.based.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Ebay_Home {
+    @Given("As an User I am on Ebay Home Page")
+    public void as_an_User_I_am_on_Ebay_Home_Page() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("I click on The Advanced Search Link")
+    public void i_click_on_The_Advanced_Search_Link() {
+        // Write code here that turns the phrase above into concrete actions
+   }
+
+    @Then("I Navigate to the Advance Search Page")
+    public void i_Navigate_to_the_Advance_Search_Page() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+
+}
---
+++ com.emids.cucumber.examples/src/test/java/com/ebay/scenario/based/test/runner/TestRunner.java
@@ -0,0 +1,12 @@
+package com.ebay.scenario.based.test.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/resources/features"},
+glue = {"com.ebay.scenario.based.steps"},
+plugin = {"pretty", "html:testreport"})
+public class TestRunner {
+}
